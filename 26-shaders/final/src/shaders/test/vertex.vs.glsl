19433cb4de1f6632f7b2b724d08fa5181e9e1e3e
Khasin <mochammadkhasin@gmail.com>
2022-05-02 19:55:37 +0700

    26-shaders 60%

---
+++ 26-shaders/final/src/shaders/test/vertex.vs.glsl
@@ -0,0 +1,18 @@
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
+
+attribute vec3 position;
+
+void main() {
+
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+  modelPosition.z = sin(modelPosition.x * 10.0) * 0.1;
+
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectedPosition; // gl_Position is the final position of the vertex
+
+  // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+}
