a28fe5e874bd572fc882496499ff77579d7d091f
Khasin <mochammadkhasin@gmail.com>
2022-05-03 09:20:13 +0700

    26-shaders 100%

--- 26-shaders/final/src/shaders/test/vertex.vs.glsl
+++ 26-shaders/final/src/shaders/test/vertex.vs.glsl
@@ -1,18 +1,37 @@
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
+uniform vec2 uFrequency;
+uniform float uTime;
 
 attribute vec3 position;
+attribute vec2 uv;
+attribute float aRandom;
+
+varying vec2 vUv;
+varying float vElevation;
+
+// varying float vRandom;
 
 void main() {
 
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.z = sin(modelPosition.x * 10.0) * 0.1;
 
+  float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+  elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+  modelPosition.z = elevation;
+
+  vElevation = elevation;
+
+  // modelPosition.z = aRandom * 0.1;
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
   gl_Position = projectedPosition; // gl_Position is the final position of the vertex
+
+  // vRandom = aRandom;
+
+  vUv = uv;
 
   // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
