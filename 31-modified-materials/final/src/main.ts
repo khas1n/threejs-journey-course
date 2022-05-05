352ecbf2928c610514132f8d08d42212706a197b
Khasin <mochammadkhasin@gmail.com>
2022-05-05 18:12:20 +0700

    31-modified-materials

---
+++ 31-modified-materials/final/src/main.ts
@@ -0,0 +1,263 @@
+import './style.scss'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import * as dat from 'lil-gui'
+import {
+  Scene,
+  TextureLoader,
+  CubeTextureLoader,
+  MeshStandardMaterial,
+  sRGBEncoding,
+  DirectionalLight,
+  PerspectiveCamera,
+  WebGLRenderer,
+  PCFShadowMap,
+  ACESFilmicToneMapping,
+  Clock,
+  Mesh,
+  PlaneGeometry,
+  RGBADepthPacking,
+  MeshDepthMaterial,
+} from 'three'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl') as HTMLElement
+
+// Scene
+const scene = new Scene()
+
+/**
+ * Loaders
+ */
+const textureLoader = new TextureLoader()
+const gltfLoader = new GLTFLoader()
+const cubeTextureLoader = new CubeTextureLoader()
+
+/**
+ * Update all materials
+ */
+const updateAllMaterials = () => {
+  scene.traverse((child) => {
+    if (child instanceof Mesh && child.material instanceof MeshStandardMaterial) {
+      child.material.envMapIntensity = 1
+      child.material.needsUpdate = true
+      child.castShadow = true
+      child.receiveShadow = true
+    }
+  })
+}
+
+/**
+ * Environment map
+ */
+const environmentMap = cubeTextureLoader.load([
+  '/textures/environmentMaps/0/px.jpg',
+  '/textures/environmentMaps/0/nx.jpg',
+  '/textures/environmentMaps/0/py.jpg',
+  '/textures/environmentMaps/0/ny.jpg',
+  '/textures/environmentMaps/0/pz.jpg',
+  '/textures/environmentMaps/0/nz.jpg',
+])
+environmentMap.encoding = sRGBEncoding
+
+scene.background = environmentMap
+scene.environment = environmentMap
+
+/**
+ * Material
+ */
+
+// Textures
+const mapTexture = textureLoader.load('/models/LeePerrySmith/color.jpg')
+mapTexture.encoding = sRGBEncoding
+
+const normalTexture = textureLoader.load('/models/LeePerrySmith/normal.jpg')
+
+// Material
+const material = new MeshStandardMaterial({
+  map: mapTexture,
+  normalMap: normalTexture,
+})
+
+const depthMaterial = new MeshDepthMaterial({
+  depthPacking: RGBADepthPacking,
+})
+
+const customUniforms = {
+  uTime: { value: 0 },
+}
+
+material.onBeforeCompile = (shader) => {
+  shader.uniforms.uTime = { value: 0 }
+  shader.uniforms.uTime = customUniforms.uTime
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <common>',
+    `
+        #include <common>
+        uniform float uTime;
+        mat2 get2dRotateMatrix(float _angle)
+        {
+            return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+        }
+    `
+  )
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <beginnormal_vertex>',
+    `
+        #include <beginnormal_vertex>
+
+        float angle = sin(position.y + uTime) * sin(position.x - uTime ) * 0.4;
+        mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+        objectNormal.xz = rotateMatrix * objectNormal.xz;
+    `
+  )
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <begin_vertex>',
+    `
+        #include <begin_vertex>
+
+        transformed.xz = rotateMatrix * transformed.xz;
+    `
+  )
+}
+
+depthMaterial.onBeforeCompile = (shader) => {
+  shader.uniforms.uTime = customUniforms.uTime
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <common>',
+    `
+            #include <common>
+
+            uniform float uTime;
+
+            mat2 get2dRotateMatrix(float _angle)
+            {
+                return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+            }
+        `
+  )
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <begin_vertex>',
+    `
+            #include <begin_vertex>
+
+            float angle = sin(position.y + uTime) * sin(position.x - uTime ) * 0.4;
+            mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+            transformed.xz = rotateMatrix * transformed.xz;
+        `
+  )
+}
+
+/**
+ * Models
+ */
+gltfLoader.load('/models/LeePerrySmith/LeePerrySmith.glb', (gltf) => {
+  // Model
+  const mesh = gltf.scene.children[0] as Mesh
+  mesh.rotation.y = Math.PI * 0.5
+  mesh.material = material
+  mesh.customDepthMaterial = depthMaterial
+  scene.add(mesh)
+
+  // Update materials
+  updateAllMaterials()
+})
+/**
+ * Plane
+ */
+const plane = new Mesh(new PlaneGeometry(15, 15, 15), new MeshStandardMaterial())
+plane.rotation.y = Math.PI
+plane.position.y = -5
+plane.position.z = 5
+scene.add(plane)
+/**
+ * Lights
+ */
+const directionalLight = new DirectionalLight('#ffffff', 3)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.normalBias = 0.05
+directionalLight.position.set(0.25, 2, -2.25)
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(4, 1, -4)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = PCFShadowMap
+renderer.physicallyCorrectLights = true
+renderer.outputEncoding = sRGBEncoding
+renderer.toneMapping = ACESFilmicToneMapping
+renderer.toneMappingExposure = 1
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new Clock()
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update material
+  customUniforms.uTime.value = elapsedTime
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
